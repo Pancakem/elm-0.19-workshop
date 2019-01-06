12640b6a556b1a335946e3de0a3e604ea60d93b2
Pancakem <pancakesdeath@protonmail.com>
2019-01-06 12:49:58 +0300

    completed part3

--- intro/part3/src/Main.elm
+++ intro/part3/src/Main.elm
@@ -88,17 +88,6 @@ viewTag selectedTagName tagName =
     in
     button
         [ class ("tag-pill " ++ otherClass)
-
-        {- 👉 TODO: Add an `onClick` handler which sends a msg
-                    that our `update` function above will use
-                    to set the currently selected tag to `tagName`.
-
-           💡 HINT: It should look something like this:
-
-                    , onClick { description = … , data = … }
-
-                    👆 Don't forget to add a comma before `onClick`!
-        -}
         , onClick { description = "ClickedTag", data = tagName } 
         ]
         [ text tagName ]
